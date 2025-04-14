b88632bd4bf8c15a908f47693e2be577bfd26ef5
Rizal Pratama <zalllrizalll012@gmail.com>
2025-04-14 07:17:10 +0700

    remove: unused packages

--- lib/components/news_category_card.dart
+++ lib/components/news_category_card.dart
@@ -1,6 +1,5 @@
 import 'package:beritaku/data/models/news_category.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class NewsCategoryCard extends StatelessWidget {
   final NewsCategory newsCategory;
